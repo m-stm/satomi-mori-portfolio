173a2fd3886cde1be4a23a5779598e2220698b1e
stm <198752589+m-stm@users.noreply.github.com>
2026-08-22 00:50:54 +0900

    fix:ファイルパスの差し替え #6

--- script.js
+++ script.js
@@ -124,8 +124,8 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       id: 1,
       title: "学習レポート共有サイト：サイトオープンのご案内",
-      thumbImage: "./images/posters/システム紹介.jpg",
-      fullImage: "./images/posters/システム紹介.jpg",
+      thumbImage: "images/webFlyer/studyReportPortal-new-release.jpg",
+      fullImage: "images/webFlyer/studyReportPortal-new-release.jpg",
       target: "就労移行支援事業所の利用者さん",
       color:
         "学習がテーマのサイトの為、知的印象持つ「グリーン」をベースカラーに採用。<br/>利用メリットはポジティブな気持ちを促す「落ち着いたピンク」をベースカラーに採用。具体的な利用場面は穏やかさをもつ「優しいブルー」をベースカラーに採用しました。<br/>全体的に統一感を残すために色のトーンが同程度になるように調整しました。",
